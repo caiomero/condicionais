96b5ce48f5e73286f6d00ec17b23e2467ff4c0a6
Caio Marinho Chiericatti <157981521+caiomero@users.noreply.github.com>
2024-10-17 19:42:48 -0300

    atividade

---
+++ app.py
@@ -0,0 +1,10 @@
+numero = int(input('digite um numero:'))
+
+if numero == 0:
+  print(f'o numero {numero} e zero.')  
+ 
+if numero < 0:
+    print(f'o numero {numero}, e negativo.')
+elif numero > 0:
+    print(f'O número {numero} é positivo.')
+    
